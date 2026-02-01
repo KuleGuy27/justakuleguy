e9c96c27a5b7dc9acdf82b3115a9402cac2e348f
KuleGuy27 <155390320+KuleGuy27@users.noreply.github.com>
2026-02-01 16:38:22 -0600

    Add files via upload

---
+++ YouTube Channels Archive/kuleguy pfp history popup.js
@@ -0,0 +1,7 @@
+function openPopup() {
+    window.open(
+        "../KuleGuy Profiles/kuleguy-profile_history.html",
+        "popupWindow",
+        "width=800,height=600,resizable=yes,scrollbars=yes"
+    );
+}
